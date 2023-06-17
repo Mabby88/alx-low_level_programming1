214e1ef5780ab6f2e524695ebc6252d461fff5c5
Mabel Mensah <mmabelzipporah@yahoo.com>
2023-06-16 21:28:16 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is
+ *  a piece of art," followed newline
+ *  Return :Always ).
+ *
+ */
+
+int main (void)
+
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
+}
